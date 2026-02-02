23baa29cddb9cfd2514224332eaef174f48280f9
Tino Mazorodze <tinomazorodze.mt@gmail.com>
2026-02-02 21:43:18 +0200

    updated to nextjs 16

---
+++ src/app/_components/index.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default function HomePageComponents() {
+  return (
+    <div className="h-screen w-screen grid place-content-center">
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl px-4 py-2 bg-black rounded-md text-white mb-6 font-bold">
+          TheWord.
+        </h1>
+        <p className="text-lg">A Christian Bible Blog.</p>
+        <aside className="mt-12 text-sm">
+          Maintained by{" "}
+          <Link href="https://zimdevelopers.com" className="underline">
+            Zim Developers Labs
+          </Link>
+          .
+        </aside>
+      </div>
+    </div>
+  );
+}
